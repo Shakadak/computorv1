65015d582b34548cb22099d4b2213967f1a9ecf2
shakadak <nathanael.pineau@gmail.com>
2015-06-04 15:03:29 +0200

    can now parse real number

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,46 @@
-mod syntax;
-mod parser_combinator;
-
-use std::env;
-use parser_combinator::{
-    Parser};
-use syntax::{
-    Digit,
-    Number};
+extern crate parser_combinators;
+use parser_combinators::{
+    Parser,
+    ParserExt,
+    many1,
+    try,
+    string,
+    optional};
+use parser_combinators::char;
+use parser_combinators::combinator;
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() == 2 {
-        println!("ok")}
-    let x = Number(Digit('3'), None);
-    match x {
-        Number(Digit(num), _)
-            => println!("{}", num)}
-    let parse_ua = Parser::literal('A');
-    let parse_y = Parser::literal('y');
-//    let parser = parse_ua.or(parse_y);
-//    let parser2 = parser.f.clone();
-    let s = "    Aytek".to_string();
-    let mut i = s.chars().peekable();
-    println!("{:?}", Parser::skip_whitespace()/*.and(Parser::option(Parser::digit())).and(Parser::skip_whitespace())*/.parse(&mut i));
-    println!("{:?}", &parse_ua.parse(&mut i));
-    println!("{:?}", &parse_ua.parse(&mut i));
-    println!("{:?}", &parse_ua.parse(&mut i));
-    println!("{:?}", &parse_ua.parse(&mut i));
-    println!("{:?}", &parse_ua.parse(&mut i));
-    println!("{:?}", &parse_ua.parse(&mut i));
-//    println!("{:?}", parser.parse(&mut i));
-//    println!("{:?}", parser.parse(&mut i));
-    println!("{:?}", i.nth(1));
-    println!("{:?}", i.peek());
-    println!("{:?}", vec![1, 2, 3]);
-    let a = Box::new(test);
-    let b = &a;
-    a();
-    b();
-}
+    let mut natural = many1::<String, _>(char::digit());
 
-fn test() {
-    println!("Hello from a closure !");}
+    let minus = char::char('-');
+
+    let relatif =
+        optional(minus.clone())
+        .and(natural.clone())
+        .map(|x| {
+            let mut ret = String::new();
+            if let Some(minus) = x.0 {
+                ret.push(minus);}
+            ret.push_str(&x.1);
+            ret});
+
+    let decimal = optional(char::char('.')
+                      .and(natural.clone())
+                      .map(|x| {
+                            let mut ret = String::new();
+                            ret.push(x.0);
+                            ret.push_str(&x.1);
+                            ret}));
+
+    let mut real = relatif.and(decimal)
+        .map(|x| {
+            let mut ret = String::new();
+            ret.push_str(&x.0);
+            if let Some(decimal) = x.1 {
+                ret.push_str(&decimal);}
+            ret.parse::<f64>().unwrap()});
+
+    println!("{:?}", natural.parse("123"));
+    println!("{:?}", real.parse("-24.5"));
+    println!("{:?}", "-34.5".to_string().parse::<f64>());
+}
--- src/syntax.rs
+++
@@ -1,8 +0,0 @@
-pub struct Letter(pub char);
-pub struct Digit(pub char);
-pub struct Power(pub char);
-pub struct Addition(pub char);
-pub struct Multiplication(pub char);
-pub struct Equality(pub char);
-
-pub struct Number(pub Digit, pub Option<Box<Number>>);
