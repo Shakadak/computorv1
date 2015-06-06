fc3f2f369405550d043a23a63db3a7834db4f6a5
shakadak <nathanael.pineau@gmail.com>
2015-06-06 22:19:49 +0200

    this approximation seems good enough

--- Math.hs
+++ Math.hs
@@ -7,3 +7,14 @@ abs n
 
 discriminant :: (Num a, Ord a) => a -> a -> a -> a
 discriminant a b c = b * b - 4 * a * c
+
+-- Square root implementation using Newton's method.
+
+sqrt :: Double -> Double
+sqrt x = head $ filter (check_root x) (iterate (improve x) 1)
+
+check_root :: Double -> Double -> Bool
+check_root target guess = Math.abs (guess * guess - target) < 0.000000001
+
+improve :: Double -> Double -> Double
+improve target prev = (prev + target / prev) / 2
