4bc202a202513b6f3d8b765b69b128c96366c545
shakadak <nathanael.pineau@gmail.com>
2015-06-06 15:57:20 +0200

    we now have a descending order polynomial

--- Main.hs
+++ Main.hs
@@ -1,6 +1,18 @@
 import Parser
 import Text.Parsec (ParseError)
+import Data.Function ((&), on)
+import Data.Ord (comparing)
+import Data.List (sortBy, groupBy)
 
-interpret_equation :: Either ParseError [(String, String)] -> IO ()
-interpret_equation (Left err) = print err
---interpret_equation
+interpret_equation :: Either ParseError [(String, String)] -> String
+interpret_equation (Left err) = show err
+--interpret_equation (Right xs) = map convert xs & simplify & solve
+
+convert :: (String, String) -> (Double, Int)
+convert (coeff, var) = (read coeff, read var)
+
+simplify :: [(Double, Int)] -> [(Double, Int)]
+simplify x = x & sortBy (flip $ comparing snd) & groupBy ((==) `on` snd) & map (foldl1 add_atom)
+
+add_atom :: (Double, Int) -> (Double, Int) -> (Double, Int)
+add_atom (x, z) (y, _) = (x + y, z)
